04f8b57e5cc5f99f15269869293ab36998c3481a
Levi <levi@teklife.info>
2016-03-25 18:49:37 -0400

    Initial commit for HungryHuntington class.

---
+++ src/HungryHuntington.php
@@ -0,0 +1,36 @@
+<?php namespace Wappr;
+
+
+/**
+ * Hungry Huntington
+ *
+ * Get all of the deals from the database and store them in a public property.
+ *
+ * @author Levi <levi@wappr.co>
+ */
+class HungryHuntington {
+    protected $db;
+
+    public function __construct() {
+        $d = self::loadConfig('db');
+        try {
+            $this->db = new \PDO("mysql:host=" . $d['hostname'] . ";dbname="
+                    . $d['database'] . ";charset=utf8",
+                    $d['username'], $d['password']);
+            $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+        } catch (Exception $ex) {
+            echo $ex->getMessage() . "\n";
+            die();
+        }
+    }
+
+    /**
+     * Load the config file
+     * 
+     * @return array configuration options for Hungry Huntington
+     */
+    public static function loadConfig() {
+        return include(ROOT . DS . 'config.php');
+    }
+}
