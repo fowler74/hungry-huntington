ad8adbfb53e9a40bc37ab1c029e01ebd990f9e03
Levi <levi@teklife.info>
2016-03-25 19:48:49 -0400

    Added twig stuff and pass the hungry object to the template.

--- public/index.php
+++ public/index.php
@@ -2,5 +2,13 @@
 date_default_timezone_set('America/New_York');
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(dirname(__FILE__)));
+
 require_once(ROOT . DS . 'src' . DS . 'HungryHuntington.php');
 require_once(ROOT . DS . 'vendor' . DS . 'autoload.php');
+
+$hungry = new Wappr\HungryHuntington;
+
+$loader = new Twig_Loader_Filesystem(ROOT . DS . 'templates');
+$twig = new Twig_Environment($loader);
+$template = $twig->loadTemplate('index.twig');
+echo $template->render(array('deals' => $hungry));
