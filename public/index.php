8fde4f39b386bdc057b44a434ec607f28b307adc
Levi <levi@teklife.info>
2016-03-26 17:36:17 -0400

    Turning off error reporting for live site.

--- public/index.php
+++ public/index.php
@@ -4,10 +4,6 @@ define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(dirname(__FILE__)));
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once(ROOT . DS . 'src' . DS . 'HungryHuntington.php');
 require_once(ROOT . DS . 'src' . DS . 'Router.php');
 require_once(ROOT . DS . 'src' . DS . 'Controller.php');
