e64a0c0ca417cc25fc0fd582bdfe4e7e73a495a4
Levi <levi@teklife.info>
2016-03-31 19:49:23 -0400

    Get deals for day of week

--- src/HungryHuntington.php
+++ src/HungryHuntington.php
@@ -159,6 +159,26 @@ class HungryHuntington {
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
+    public function getDealsForDow($dow) {
+        $query = 'SELECT d.headline, d.url_title as deal_url, d.description, c.name,
+            c.google_map, c.website, c.phone, c.address, t.type_of_deal,
+            c.url_title
+            FROM deals d
+            JOIN companies c
+            ON c.company_id = d.company_id
+            LEFT JOIN types_of_deals t
+            ON t.type_id = d.type_id
+            JOIN dow_deal b
+            ON d.id = b.deals_id_fk
+            JOIN days_of_week dow
+            ON dow.dow_id = b.dow_id_fk
+            WHERE dow.dow_name = :dow
+            AND d.deleted = 0';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
     public function getTypes() {
         $query = 'SELECT type_id, type_of_deal
         FROM types_of_deals
