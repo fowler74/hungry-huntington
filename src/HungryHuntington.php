7f9f801de10a8b06ac4051bd9d4dc5df0c9e8ac6
Levi <levi@teklife.info>
2016-03-31 20:40:16 -0400

    adding random deals

--- src/HungryHuntington.php
+++ src/HungryHuntington.php
@@ -67,6 +67,28 @@ class HungryHuntington {
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
+    public function getRandomDeal($type) {
+        $query = 'SELECT id, c.name, headline, description,
+        c.google_map, c.website, c.phone, c.address, t.type_of_deal,
+        c.url_title, d.url_title as deal_url
+        FROM deals d
+        LEFT JOIN companies c
+        ON c.company_id = d.company_id
+        LEFT JOIN types_of_deals t
+        ON t.type_id = d.type_id
+        WHERE d.deleted = 0';
+        if($type == 'drink-deals') {
+            $query .= 'AND t.type_id = 3';
+        } elseif($type == 'food-deals') {
+            $query .= 'AND t.type_id != 3';
+        }
+        $query .= 'ORDER BY RAND()
+        LIMIT 1';
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
     public function getTodaysDeals() {
         $dow = date("l");
         $query = 'SELECT d.headline, d.url_title as deal_url, d.description, c.name,
