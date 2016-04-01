4dd3f28abc01f47c4cbc794ade9ab7b795d6a0f7
Levi <levi@teklife.info>
2016-03-31 20:12:22 -0400

    Removing old jquery

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -19,33 +19,4 @@ $(document).ready(function(){
     $('.li-open').click(function(){
         $(this).children().slideToggle();
     });
-
-    //NEED HELP HERE!! (make it better :P )
-    $('nav li').click(function(){
-        /*event.preventDefault();*/
-    });
-    $('.daily-li').click(function(){
-        $('.daily').removeClass('none').addClass('block');
-        $('.weekly').addClass('none');
-        $('.bar').addClass('none');
-    });
-    $('.weekly-li').click(function(){
-        $('.weekly').removeClass('none').addClass('block');
-        $('.daily').addClass('none');
-        $('.bar').addClass('none');
-    });
-    $('.bar-li').click(function(){
-        $('.bar').removeClass('none').addClass('block');
-        $('.weekly').addClass('none');
-        $('.daily').addClass('none');
-    });
-    $('.random-li').click(function(){
-        maxDeals = $('.deal').length;
-        random = Math.floor((Math.random() * maxDeals) + 1);
-        $('.deal').addClass('none');
-        $('.deal').removeClass('block');
-        $('.deal:nth-of-type(' + random + ')').addClass('block');
-        $('.deal:nth-of-type(' + random + ')').removeClass('none');
-        console.log(random);
-    });
 });
