3f68afb1ae1c13cfbfaa800158800ee94e59550d
Levi <levi.durfee@gmail.com>
2016-05-29 10:55:15 -0400

    [#20] update restaurant listings

--- src/HungryHuntington.php
+++ src/HungryHuntington.php
@@ -237,6 +237,30 @@ class HungryHuntington {
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
 
+    public function getCompanyDealsGrouped($companyUrl) {
+        $grouped = array();
+        $deals = array();
+        $query = 'SELECT d.headline, d.url_title as deal_url, d.description, c.name,
+        c.google_map, c.website, c.phone, c.address, t.type_of_deal,
+        c.directions, c.url_title
+        FROM deals d
+        JOIN companies c
+        ON c.company_id = d.company_id
+        LEFT JOIN types_of_deals t
+        ON t.type_id = d.type_id
+        WHERE d.deleted = 0
+        AND c.url_title = :url_title';
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':url_title', $companyUrl, \PDO::PARAM_STR);
+        $stmt->execute();
+        $deals = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        // make the restaurant name the key of the array
+        for($i=0;$i<count($deals);$i++) {
+            $grouped[$deals[$i]['name']][] = $deals[$i];
+        }
+        return $grouped;
+    }
+
     public function getDeal($companyUrl, $dealUrl) {
         $query = 'SELECT d.headline, d.url_title as deal_url, d.description, c.name,
         c.google_map, c.website, c.phone, c.address, t.type_of_deal,
