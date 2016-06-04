b5d5e8a47d9612a39582c68ebc5e581204d1f527
Levi <levi.durfee@gmail.com>
2016-06-04 12:52:24 -0400

    Add route for food trucks

--- routes.php
+++ routes.php
@@ -6,3 +6,4 @@ $router->add('weekly', 'weekly', ['getWeek', 'getDay'], true);
 $router->add('bar', 'bar');
 $router->add('random', 'random', ['getRandom', 'getRandomDeal'], true);
 $router->add('submit', 'submit', ['doNothing', 'doNothing'], true);
+$router->add('food-trucks', 'food-trucks', ['getTruckPage', 'getTruckPage'], true);
